798195c27f32c3f803b5bb4d938bff3121491537
Maximilian Babeluk <maximilian.babeluk@oeaw.ac.at>
2025-03-30 09:22:30 +0200

    Even slower ramping for Giuliana

--- src/bdaq_supply.py
+++ src/bdaq_supply.py
@@ -35,18 +35,21 @@ class PowerManager():
         if not self.ps.allOff() or self.smu.isOn():
             if self.ch_psubwell.isOn():
                 vstart = self.ch_psubwell.measVoltage()
-                for v in np.arange(vstart, 0., -1):
+                for v in np.arange(vstart, 0., -0.5):
                     self.ch_psubwell.setVoltage(v)
+                    time.sleep(0.1)
                 self.ch_psubwell.setVoltage(0)
             if self.ch_pwell.isOn():
                 vstart = self.ch_pwell.measVoltage()
-                for v in np.arange(vstart, 0., -1):
+                for v in np.arange(vstart, 0., -0.5):
                     self.ch_pwell.setVoltage(v)
+                    time.sleep(0.1)
                 self.ch_pwell.setVoltage(0)
             if self.smu.isOn():
                 vstart = self.smu.get_voltage()
-                for v in np.arange(vstart, 0., -1):
+                for v in np.arange(vstart, 0., -0.5):
                     self.smu.set_voltage(v)
+                    time.sleep(0.1)
                 self.smu.set_voltage(0)
             
             self.shutdown(before=True)
@@ -119,15 +122,17 @@ class PowerManager():
 
     def rampDown(self):
         if(self.psubwell != 0):
-            for v in np.arange(self.psubwell, 0., -1):
+            for v in np.arange(self.psubwell, 0., -0.5):
                 self.ch_psubwell.setVoltage(v)
+                time.sleep(0.1)
             self.ch_psubwell.setVoltage(0)
         if(self.pwell != 0):
-            for v in np.arange(self.pwell, 0., -1):
+            for v in np.arange(self.pwell, 0., -0.5):
                 self.ch_pwell.setVoltage(v)
+                time.sleep(0.1)
             self.ch_pwell.setVoltage(0)
         if(self.hv != 0):
-            for v in np.arange(self.hv, 0, -1):
+            for v in np.arange(self.hv, 0, -0.5):
                 self.smu.set_voltage(v)
                 time.sleep(0.5)
             self.smu.set_voltage(0)
@@ -137,13 +142,15 @@ class PowerManager():
         if(self.pwell != 0):
             for v in np.arange(0., self.pwell, 0.5):
                 self.ch_pwell.setVoltage(v)
+                time.sleep(0.1)
             self.ch_pwell.setVoltage(self.pwell)
         if(self.psubwell != 0):
             for v in np.arange(0., self.psubwell, 0.5):
                 self.ch_psubwell.setVoltage(v)
+                time.sleep(0.1)
             self.ch_psubwell.setVoltage(self.psubwell)
         if(self.hv != 0):
-            for v in np.arange(0., self.hv, 1):
+            for v in np.arange(0., self.hv, 0.5):
                 self.smu.set_voltage(v)
                 time.sleep(0.5)
             self.smu.set_voltage(self.hv)
